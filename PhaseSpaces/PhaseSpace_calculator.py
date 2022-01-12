024d461b1220285b93d0e36f15154d24ecbe5b1f
TabeaHeckenthaler <tabea@heckenthaler.de>
2022-01-12 20:42:59 +0200

    Wrote a todo list

--- PhaseSpaces/PhaseSpace_calculator.py
+++ PhaseSpaces/PhaseSpace_calculator.py
@@ -14,3 +14,10 @@ for size in exp_types[shape][solver]:
     # conf_space_labeled.visualize_transitions(reduction=10)
 
     DEBUG = 1
+
+# TODO what is going on with smooth connector. See step by step in what direction he is moving.
+#  Can I display the smooth connector?
+#  Can I put the two together?
+#  When I display the movies, does if help if I shift the center of mass?
+#  If it does, you have to recalculate the Phase Space.
+
