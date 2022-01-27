eeca2d3f389fb572a79aea8fbd338f2b8a69fc2a
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-01-27 11:08:51 +0200

    changed module names

--- Load_tracked_data/post_loading_checking_correcting.py
+++ Load_tracked_data/post_loading_checking_correcting.py
@@ -1,24 +1,24 @@
 from trajectory_inheritance.trajectory import get
 from Load_tracked_data.Load_Experiment import load, connector, parts, time_dict, winner_dict
 import numpy as np
-import json
-
-# TODO: Correct these.
+import os
 
 
 def to_interpolate():
     interpolate_list = {
         'S_SPT_4720005_SSpecialT_1_ants (part 1)': [],
-        # cut off at 49704
+    }
+    easy_interpolate_list = {
+        'S_SPT_4720005_SSpecialT_1_ants (part 1)': [],
     }
 
     for filename, exclude in interpolate_list.items():
         x = get(filename)
         print(x)
-        # new = x.easy_interpolate([[28471, 31179]])
-        # new = new.interpolate(exclude)
-        x.play(frames=[0, -1])
-        x.save()
+        new = x.easy_interpolate(easy_interpolate_list[filename])
+        new = new.interpolate(exclude)
+        new.play(frames=[0, -1])
+        new.save()
 
 
 def faulty_connection():
@@ -41,6 +41,32 @@ def faulty_connection():
             x = x + part
         x.play(step=5)
         x.save()
+
+
+def delete_from_file(deletable_line):
+    with open("check_trajectories.txt", "r") as input_:
+        with open("temp.txt", "w") as output_:
+            for line in input_:
+                if line.strip("\n").split(':')[0] != deletable_line:
+                    output_.write(line)
+    os.replace('temp.txt', 'check_trajectories.txt')
+
+
+def play_trajectories():
+    file1 = open('check_trajectories.txt', 'r')
+    trajectories_filenames = [line.split(':')[0].replace('\n', '')
+                              for line in file1.readlines() if len(line) > 5]
+    file1.close()
+
+    start = 18
+    for i, trajectories_filename in enumerate(trajectories_filenames[start:], start=start):
+        print(trajectories_filename)
+        print(i)
+        x = get(trajectories_filename)
+        x.play(step=1)
+
+        if bool(int(input('OK? '))):
+            delete_from_file(trajectories_filename)
 
 
 if __name__ == '__main__':
