cb971980b3ae642bfb565413ee1d03da6898d1fb
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-02-08 09:24:37 +0200

    More renovations

--- PS_Search_Algorithms/Path_Planning_Rotation_students.py
+++ PS_Search_Algorithms/Path_Planning_Rotation_students.py
@@ -1,12 +1,13 @@
 from copy import copy
-
+import os
 import numpy as np
 from skfmm import travel_time
 from typing import Union
 
 from PS_Search_Algorithms.Path_planning_in_CS import Path_planning_in_CS, Node3D, Node2D
 from ConfigSpace.ConfigSpace_Maze import ConfigSpace
-from trajectory_inheritance.trajectory_ps_simulation import Trajectory_ps_simulation
+import pandas as pd
+from Directories import home
 
 
 class Binned_ConfigSpace(ConfigSpace):
@@ -17,42 +18,32 @@ class Binned_ConfigSpace(ConfigSpace):
 
     def calculate_binned_space(self) -> np.array:
         """
-        :return: Space that has
+        :return: np.array that has dimensions self.space.shape/self.resolution, which says, how much percent coverage the
+        binned space has.
         """
-        return np.array([])
-
-    def bins_connected(self, ind1, ind2) -> bool:
-        """
-
-        :param ind1: indices in self.space
-        :param ind2: indices in self.space
-        :return: Whether there is a path from ind1 to ind2 without leaving the bins.
-        """
-        return self.find_path(ind1, ind2)[0]
-
-    def bin_ind(self, ind: tuple) -> tuple:
         # TODO
-        return ind
+        return np.array([])
 
     def bin_cut_out(self, indices: list) -> np.array:
         """
         Return only the cut out part where all the indices lie in. The indices have to be in adjacent bins.
-        :param indices:
+        :param indices: indices in self.space
         :return:
         """
+        # TODO
         return np.array([])
 
-    def find_path(self, ind1, ind2) -> tuple:
+    def find_path(self, ind1: tuple, ind2: tuple) -> tuple:
         """
 
         :param ind1: indices of first node (in high_resolution_space)
         :param ind2: indices of second node (in high_resolution_space)
         :return: (boolean (whether a path was found), list (node indices that connect the two indices))
         """
-        # TODO: check whether centers of voxels are connected.
         bins = self.bin_cut_out([ind1, ind2])
         # path only within the adjacent bins
-        Planner = Path_planning_in_CS(Node2D(*ind1, self.space), Node2D(*ind2, self.space), conf_space=bins, )
+        # TODO Tabea: I think, we have to change the indices to new cut out.
+        Planner = Path_planning_in_CS(Node2D(*ind1, bins), Node2D(*ind2, bins), conf_space=bins)
         Planner.path_planning()
 
         if Planner.winner:
@@ -66,9 +57,9 @@ class Path_Planning_Rotation_students(Path_planning_in_CS):
                  max_iter: int = 100000, dil_radius: int = 0):
         super().__init__(start, end, max_iter, conf_space=conf_space)
         self.dil_radius = dil_radius
-        self.speed = self.initialize_speed()
         self.resolution = resolution
-        # TODO Tabea: Greedy node on high resolution conf_space!!!!
+        self.speed = self.initialize_speed()
+        DEBUG = 1
 
     def step_to(self, greedy_node) -> None:
         greedy_node.parent = copy([self.known_conf_space.find_path(self.current.ind(), greedy_node.ind())[1]])
@@ -85,19 +76,20 @@ class Path_Planning_Rotation_students(Path_planning_in_CS):
         self.known_conf_space.space = Binned_ConfigSpace(self.known_conf_space.space, self.resolution)
 
     def initialize_speed(self) -> np.array:
-        return 1/Binned_ConfigSpace(self.conf_space, self.resolution).binned_space
+        return Binned_ConfigSpace(self.conf_space, self.resolution).binned_space
 
-    def possible_step(self, greedy_node: Node3D) -> bool:
-        if self.known_conf_space.bins_connected(self.current.ind(), greedy_node.ind()):
+    def possible_step(self, greedy_node: Union[Node2D, Node3D]) -> Union[bool, list]:
+        path_exists, path = self.known_conf_space.bins_connected(self.current.ind(), greedy_node.ind())
+        if path_exists:
             manage_to_pass = np.random.uniform() > self.known_conf_space.coverage()[greedy_node.ind()]
             if manage_to_pass:
-                return False
+                return path
             else:
                 return True
         else:
             return False
 
-    def add_knowledge(self, central_node: Node3D) -> None:
+    def add_knowledge(self, central_node: Union[Node2D, Node3D]) -> None:
         """
         No path was found in greedy node, so we need to update our self.speed.
         Some kind of Bayesian estimation.
@@ -128,3 +120,13 @@ class Path_Planning_Rotation_students(Path_planning_in_CS):
         # TO CHECK: how to plot your results in 2D in a certain plane
         # plot_distances(self, index=self.current.xi, plane='x')
 
+
+if __name__ == '__main__':
+    directory = os.path.join(home, 'PS_Search_Algorithms', 'path_planning_test.xlsx')
+    conf_space = pd.read_excel(io=directory, sheet_name='space').to_numpy()
+    resolution = 2
+
+    Planner = Path_Planning_Rotation_students(conf_space=ConfigSpace(space=conf_space),
+                                              start=Node2D(0, 0, conf_space),
+                                              end=Node2D(8, 8, conf_space),
+                                              resolution=resolution)
--- PS_Search_Algorithms/Path_Planning_Rotation_students_test.py
+++ PS_Search_Algorithms/Path_Planning_Rotation_students_test.py
@@ -1,42 +1,32 @@
 import unittest
-import os
 
 from PS_Search_Algorithms.Path_Planning_Rotation_students import *
 import pandas as pd
-from Directories import home
 from ConfigSpace.ConfigSpace_Maze import ConfigSpace
 
-dir = os.path.join(home, 'PS_Search_Algorithms', 'path_planning_test.xlsx')
-space = pd.read_excel(io=dir, sheet_name='space').to_numpy()
+
+# Zeile, Spalte
 binned_space = pd.read_excel(io=dir, sheet_name='binned_space').to_numpy()
-resolution = 2
+cs = ConfigSpace(conf_space)
+binned_cs = Binned_ConfigSpace(config_space=cs, resolution=resolution)
+Planner = Path_Planning_Rotation_students(conf_space=ConfigSpace(space=conf_space),
+                                          start=Node2D(0, 0, conf_space),
+                                          end=Node2D(8, 8, conf_space),
+                                          resolution=resolution)
 
 
 class Path_PlanningTest(unittest.TestCase):
     def test_calculate_binned_space(self):
-        cs = ConfigSpace(space)
-        binned_cs = Binned_ConfigSpace(config_space=cs, resolution=resolution)
         self.assertEqual(binned_cs.binned_space, binned_space)
 
-    def test_bins_connected(self):
-        self.assertEqual(
-
-        )
+    def test_bin_cut_out(self):
+        self.assertEqual(binned_cs.bin_cut_out([(0, 0), (2, 3)]), binned_space[0:4, 1:3])
 
     def test_find_path(self):
-        self.assertEqual(
-
-        )
-
-    def test_step_to(self):
-        self.assertEqual(
-
-        )
+        self.assertEqual(binned_cs.find_path((3, 0), (0, 5))[0], True)
 
     def test_initialize_speed(self):
-        self.assertEqual(
-
-        )
+        self.assertEqual(Planner.speed, binned_cs.binned_space)
 
     def test_add_knowledge(self):
         self.assertEqual(
