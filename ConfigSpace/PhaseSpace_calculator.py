43952470e81c4f508a34bf65093e4537b370bd0b
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-03-13 16:47:59 +0200

    nothing

--- ConfigSpace/PhaseSpace_calculator.py
+++ ConfigSpace/PhaseSpace_calculator.py
@@ -3,12 +3,13 @@ from trajectory_inheritance.exp_types import exp_types
 
 shape = 'SPT'
 solver = 'human'
-sizes = ['Small Far'] # exp_types[shape][solver]
+sizes = ['Small Far']  # exp_types[shape][solver]
 
 for size in sizes:
-    conf_space = ConfigSpace_Maze.ConfigSpace_Maze(solver, size, shape, ('MazeDimensions_human.xlsx', 'LoadDimensions_human.xlsx'))
+    conf_space = ConfigSpace_Maze.ConfigSpace_Maze(solver, size, shape,
+                                                   ('MazeDimensions_human.xlsx', 'LoadDimensions_human.xlsx'))
     conf_space.calculate_space()
-    conf_space.load_space()
+    # conf_space.load_space()
     conf_space.visualize_space()
     # conf_space.calculate_boundary()
     # conf_space.save_space()
