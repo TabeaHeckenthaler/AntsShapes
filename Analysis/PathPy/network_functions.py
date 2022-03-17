cbd14e7849a1a7e4ca0058ab685e5125460c1337
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-03-17 16:25:39 +0200

    Added cut off of states after reaching the final state.

--- Analysis/PathPy/network_functions.py
+++ Analysis/PathPy/network_functions.py
@@ -1,60 +1,35 @@
 import pathpy as pp
-from DataFrame.dataFrame import myDataFrame
-from trajectory_inheritance.trajectory import get
+from pathpy.visualisation import export_html
 from matplotlib import pyplot as plt
 import igraph
-from pathpy.visualisation import export_html
 from Directories import network_dir
-from os import path
-from Analysis.PathPy.AbsorbingMarkovChain import *
-from Analysis.GeneralFunctions import graph_dir
+from trajectory_inheritance.exp_types import exp_types
 import os
 from copy import copy
 import json
+from Analysis.GeneralFunctions import graph_dir
 from Analysis.States import states, forbidden_transition_attempts, allowed_transition_attempts
-import pandas as pd
-
-
-def get_trajectories(solver='human', size='Large', shape='SPT',
-                     geometry: tuple = ('MazeDimensions_human.xlsx', 'LoadDimensions_human.xlsx'), number: int = None) \
-        -> list:
-    """
-    Get trajectories based on the trajectories saved in myDataFrame.
-    :param geometry: geometry of the maze, given by the names of the excel files with the dimensions
-    :param number: How many trajectories do you want to have in your list?
-    :param solver: str
-    :param size: str
-    :param shape: str
-    :return: list of objects, that are of the class or subclass Trajectory
-    """
-    df = myDataFrame[
-        (myDataFrame['size'] == size) &
-        (myDataFrame['shape'] == shape) &
-        (myDataFrame['solver'] == solver) &
-        (myDataFrame['initial condition'] == 'back') &
-        (myDataFrame['maze dimensions'] == geometry[0]) &
-        (myDataFrame['load dimensions'] == geometry[1])]
-
-    filenames = df['filename'][:number]
-    # filenames = ['XL_SPT_dil9_sensing' + str(ii) for ii in [5, 6, 7, 8, 9]]
-    return [get(filename) for filename in filenames]
-
+from Analysis.PathPy.AbsorbingMarkovChain import *
+from Analysis.PathPy.Paths import Paths
 
 sizes = {0: 5.0, 1: 10.0, 2: 5.0}
 colors = {0: 'black', 1: 'red', 2: 'blue'}
 
 
 class Network(pp.Network):
-    def __init__(self, solver, size, shape, possible_transitions: list = None):
+    def __init__(self, solver: str, size: str, shape: str, paths: Paths):
         super().__init__(directed=True)
+        if 'Small' in size:
+            size = 'Small'
         self.name = '_'.join(['network', solver, size, shape])
-        self.paths = pp.Paths()
-        self.T = None
-        self.N = None
+        self.paths = paths
+        self.add_edges()
+        self.T = None  # transition matrix
+        self.N = None  # fundamental matrix
         self.Q = None
-        self.t = None
+        self.t = None  # absorption time
         self.R = None
-        self.P = None
+        self.P = None  # canonical form of transition matrix
         self.B = None
         # if possible_transitions is not None:
         #     for state1, state2 in itertools.product(possible_transitions, possible_transitions):
@@ -62,41 +37,10 @@ class Network(pp.Network):
 
     def to_dict(self) -> dict:
         if self.N is None:
-            self.calc_fundamental_matrix()
-        if self.t is None:
-            self.calc_expected_absorption_time()
-        if self.B is None:
-            self.calc_absorption_probabilities()
+            self.markovian_analysis()
 
-        return {'T': self.T.tolist(), 'N': self.N.tolist(), 'Q': self.Q.tolist(), 't': self.t.tolist(),
-                'R': self.R.tolist(), 'P': self.P.tolist(), 'B': self.B.tolist(), 'state_order': list(self.nodes.keys())}
-
-    def create_paths(self):
-        raise ValueError('You still have to program this!')
-        # conf_space_labeled = ConfigSpace_Maze.PhaseSpace_Labeled(solver, size, shape, geometry)
-        # conf_space_labeled.load_labeled_space()
-        # conf_space_labeled.visualize_states()
-        # trajectories = get_trajectories(solver=solver, size=size, shape=shape, geometry=geometry)
-        # list_of_states = [States(conf_space_labeled, x, step=int(x.fps)) for x in trajectories]
-        # transitions = [s.combine_transitions(s.state_series) for s in list_of_states]
-        # self.add_paths(transitions)
-
-    def save_dir_paths(self):
-        return os.path.join(network_dir, 'ExperimentalPaths', self.name + '_transitions.txt')
-
-    def save_paths(self):
-        raise ValueError('Check save_paths')
-        with open(self.save_dir_paths(), 'w') as json_file:
-            json.dump(self.paths, json_file)
-
-    def get_paths(self):
-        if os.path.exists(self.save_dir_paths()):
-            with open(self.save_dir_paths(), 'r') as json_file:
-                transitions = json.load(json_file)
-            self.add_paths(transitions)
-        else:
-            self.create_paths()
-            self.save_paths()
+        return {'T': self.T.to_json(), 'N': self.N.to_json(), 'Q': self.Q.to_json(), 't': self.t.to_json(),
+                'R': self.R.to_json(), 'P': self.P.to_json(), 'B': self.B.to_json()}
 
     def save_dir_results(self):
         return os.path.join(network_dir, 'MarkovianNetworks', self.name + '.txt')
@@ -110,20 +54,18 @@ class Network(pp.Network):
         if os.path.exists(self.save_dir_results()):
             with open(self.save_dir_results(), 'r') as json_file:
                 attribute_dict = json.load(json_file)
-            self.N = pd.DataFrame(attribute_dict['N'])
-            self.Q = pd.DataFrame(attribute_dict['Q'])
-            self.t = pd.DataFrame(attribute_dict['t'])
-            self.R = pd.DataFrame(attribute_dict['R'])
-            self.P = pd.DataFrame(attribute_dict['P'])
-            self.B = pd.DataFrame(attribute_dict['B'])
+            self.T = pd.read_json(attribute_dict['T'])
+            self.N = pd.read_json(attribute_dict['N'])
+            self.Q = pd.read_json(attribute_dict['Q'])
+            self.t = pd.read_json(attribute_dict['t'])
+            self.R = pd.read_json(attribute_dict['R'])
+            self.P = pd.read_json(attribute_dict['P'])
+            self.B = pd.read_json(attribute_dict['B'])
         else:
-            self.get_paths()
-            results = self.to_dict()
-            self.save_results(results)
+            self.markovian_analysis()
+            self.save_results(self.to_dict())
 
-    def add_paths(self, transitions: list) -> None:
-        [self.paths.add_path(transition) for transition in transitions]
-
+    def add_edges(self):
         for states, weight in self.paths.paths[1].items():
             if len(states) == 2:
                 self.add_edge(states[0], states[1], weight=weight[0])
@@ -142,7 +84,7 @@ class Network(pp.Network):
                         "node_size": {name: sizes[len(name)] for name in g1.vs["name"]},
                         "node_color": {name: colors[len(name)] for name in g1.vs["name"]},
                         }
-        directory = path.join(network_dir, 'Network_Images', self.name + '.html')
+        directory = os.path.join(network_dir, 'Network_Images', self.name + '.html')
         print('Saving network image in ', directory)
         export_html(n_to_plot, directory, **visual_style)
 
@@ -178,15 +120,13 @@ class Network(pp.Network):
 
     def plot_transition_matrix(self, title: str = '', axis=None):
         if self.T is None:
-            self.T = self.transition_matrix().toarray()
+            self.markovian_analysis()
         if axis is None:
             fig, axis = plt.subplots(1, 1)
         _ = axis.imshow(self.T)
-        axis.set_xticks(range(len(self.nodes.keys())))
-        axis.set_yticks(range(len(self.nodes.keys())))
-        axis.set_xticklabels(self.nodes.keys())
-        axis.set_yticklabels(self.nodes.keys())
-        directory = graph_dir() + os.path.sep + 'T_' + self.name + '.pdf'
+        plt.xticks(range(len(self.T)), self.T.columns)
+        plt.yticks(range(len(self.T)), self.T.columns)
+        directory = graph_dir() + os.path.sep + 'transition_matrix_' + self.name + '.pdf'
         print('Saving transition matrix in ', directory)
         plt.gcf().savefig(directory)
         plt.title(title)
@@ -220,35 +160,34 @@ class Network(pp.Network):
         # nothing to sort, return
         return m
 
-    def calc_fundamental_matrix(self):
+    def markovian_analysis(self):
         self.T = pd.DataFrame(self.transition_matrix().toarray(),
                               columns=list(self.node_to_name_map()),
                               index=list(self.node_to_name_map()))
-        sorted = self.sort(self.T.transpose())
-        self.T = pd.DataFrame(normalize(sorted.to_numpy()),
-                              columns=sorted.columns, index=sorted.columns)
+        sorted_transition_matrix = self.sort(self.T.transpose())
+        self.T = pd.DataFrame(normalize(sorted_transition_matrix.to_numpy()),
+                              columns=sorted_transition_matrix.columns,
+                              index=sorted_transition_matrix.columns)
         number_of_transient_states = num_of_transients(self.T)
+        number_of_absorbing_states = self.T.shape[0] - number_of_transient_states
+        transient_state_order = sorted_transition_matrix.columns[:number_of_transient_states]
 
         self.Q, self.R = decompose(self.T)
-        absorbing_states = 1
-        self.P = np.vstack([np.hstack([identity(absorbing_states),
-                                       np.zeros([absorbing_states, number_of_transient_states])]),
-                            np.hstack([self.R, self.Q])])
-        # canonical form of transition matrix
+        self.P = pd.DataFrame(np.vstack([np.hstack([identity(number_of_absorbing_states),
+                                         np.zeros([number_of_absorbing_states, number_of_transient_states])]),
+                              np.hstack([self.R, self.Q])]),
+                              columns=sorted_transition_matrix.columns,
+                              index=sorted_transition_matrix.columns)
+        self.N = pd.DataFrame(np.linalg.inv(identity(self.Q.shape[-1]) - self.Q),
+                              columns=transient_state_order,
+                              index=transient_state_order)  # fundamental matrix
+        self.B = pd.DataFrame(np.matmul(self.N, self.R))  # absorption probabilities
+        self.t = pd.DataFrame(np.matmul(self.N, np.ones(self.N.shape[0])), index=transient_state_order)
+
+    def check_P(self):
         # P_inf = np.linalg.matrix_power(self.P, 100)  # check whether P_inf is indeed np.array([[I, 0], [B, 0]])
         # print(P_inf)
-        self.N = np.linalg.inv(identity(len(self.Q[-1])) - self.Q)  # fundamental matrix
-
-    def calc_expected_absorption_time(self):
-        if self.N is None:
-            self.calc_fundamental_matrix()
-        self.t = np.matmul(self.N,
-                           np.ones(self.N.shape[0]))  # expected number of steps before absorption from each steps
-
-    def calc_absorption_probabilities(self):
-        if self.N is None:
-            self.calc_fundamental_matrix()
-        self.B = np.matmul(self.N, self.R)  # absorption probabilities
+        pass
 
     # def create_higher_order_network(self, k: int = 2) -> pp.Network:
     #     hon = pp.HigherOrderNetwork(self.paths, k=k, null_model=True)
@@ -256,15 +195,6 @@ class Network(pp.Network):
     #     #     print(e, hon.edges[e])
     #     return hon
     #
-
-    # def Markovian_analysis(self) -> np.array:
-    #     # adjacency_matrix, degrees, laplacian, transition matrix and eigenvector
-    #     A = self.adjacency_matrix().toarray()
-    #     D = np.eye(len(self.degrees())) * np.array(self.degrees())
-    #     L = self.laplacian_matrix(self.degrees()).toarray()
-    #     T = self.transition_matrix().toarray()
-    #     EV = self.leading_eigenvector(self.adjacency_matrix())
-    #     return T
     #
     # def higher_order_networks(self):
     #     hon = self.create_higher_order_network()
@@ -286,13 +216,12 @@ class Network(pp.Network):
 
 if __name__ == '__main__':
     solver, shape, geometry = 'human', 'SPT', ('MazeDimensions_human.xlsx', 'LoadDimensions_human.xlsx')
+    nodes = sorted(states + forbidden_transition_attempts + allowed_transition_attempts)
 
-    sizes = ['Large', 'Medium']
-    for size in sizes:
-        state_order = sorted(states + forbidden_transition_attempts + allowed_transition_attempts)
-        my_network = Network(solver, size, shape, possible_transitions=state_order)
+    for size in exp_types[shape][solver]:
+        paths = Paths(solver, size, shape, geometry)
+        paths.load_paths()
+        my_network = Network(solver, size, shape, paths)
         my_network.get_results()
         my_network.plot_transition_matrix()
         DEBUG = 1
-
-        # my_network.plot_network()
